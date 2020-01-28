cc098e50dfed2334624c2677f21bfcee64937b6d
Arika Afrin Boshra <almustakim@Als-Mac-mini.local>
2020-01-28 14:55:13 +0600

    this is a test

---
+++ WeatherAppICT/test/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  WeatherAppICT
+//
+//  Created by Arika Afrin Boshra on 28/1/20.
+//  Copyright © 2020 Md Zahidul Islam Mazumder. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
